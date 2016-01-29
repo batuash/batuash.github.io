33a68063d6030ea889a8ce87e8b2387a5da1697c
Batuash <adambat1899@gmail.com>
2016-01-29 15:45:01 +0100

    Locatfox technical test

---
+++ Locafox/scripts/controllers/homeController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    var module = angular.module("app");
+
+    module.controller("homeController", ['$scope', 'webApiService', function ($scope, webApiService) {
+        var init = function () {
+            webApiService.getMapLocations().then(function (response) {
+                if (response.success) {
+                    $scope.populateMap(response.data);
+                }
+                else {
+                    console.log("Could not fetch data!!");
+                }
+            });
+        }
+
+        init();
+    }]);
+})();
---
+++ Locafox/scripts/directive/maplocationsDirective.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    var module = angular.module("app");
+
+    module.directive('maplocationsDirective', function () {
+        return {
+            restrict: 'E',
+            template: '<div class="mapLocations"></div>',
+            link: function (scope, element, attr) {
+                scope.populateMap = function (mapLocations) {
+                    if (!mapLocations[0])
+                        return;
+                    var myCenter = new google.maps.LatLng(mapLocations[0].lat, mapLocations[0].long);
+                    var mapProp = {
+                        center: myCenter,
+                        zoom: 11,
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    };
+
+                    var map = new google.maps.Map(element.find(".mapLocations")[0], mapProp);
+
+                    var location, marker;
+                    for (var i = 0; i < mapLocations.length; i++) {
+                        var location = new google.maps.LatLng(mapLocations[i].lat, mapLocations[i].long);
+                        var marker = new google.maps.Marker({
+                            position: location,
+                        });
+
+                        marker.setMap(map);
+                    }
+                }
+            }
+        };
+    });
+})();
